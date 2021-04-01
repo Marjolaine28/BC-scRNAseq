39e7e3d6818341f2745f0f1bbcdce9905e02ac96
Marjolaine28 <marjolaine.david28@gmail.com>
2021-04-01 04:40:16 -0400

    Fixed merging fastqs script

---
+++ scripts/bash/quantification/run-salmon.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+
+
+####### HELP PANEL #######
+
+if [[ $1 = '--help' ]]
+then
+    echo "
+
+
+--------------- HELP ---------------
+
+
+
+
+Script    $(basename $0)    must be run with a :
+
+
+
+-f : input_r1 = path to fastq read 1
+
+-o : output_folder = path where you want to store the quantificaton results
+
+-l : sequencing library type, can be 'PE' (for paired end) or 'SE' (for single end) 
+
+-p : path to salmon pipeline
+
+-args : other alevin parameters (e.g. index, txp2gene file, --dropseq).
+
+-emp : If set to 1, creates empty output files and folders. Not implemented yet, just exits when called.
+
+
+
+
+"
+        exit 0
+fi
+
+
+
+#######  ARGUMENTS #######
+
+while [[ $# -gt 0 ]];
+do
+    opt="$1";
+    shift; 
+    case "$opt" in       
+        -f) input_r1="$1"; shift;;            
+        -o) output_folder="$1"; shift;;
+        -l) lib="$1"; shift;;
+        -p) salmon="$1"; shift;;
+        -args) args="$1"; IFS=' ' declare -a 'args=($args)'; shift;;
+        -emp) emp="$1"; shift;; 
+    esac
+done
+
+
+####### EMPTY MODE TO ONLY CREATE EMPTY OUTPUT FILES AND FOLDERS -- useful for qsub-all-fastq.sh #######
+# No empty mode implemented ; not needed for now (no holding job waiting for it)
+
+emp=${emp:-'0'}             # 0 by default
+
+if [[ $emp = 1 ]]
+then
+	exit 1
+fi
+
+
+####### IF A PATH TO SALMON IS SET WITH -P, ADD IT TO $PATH #######
+
+if [[ -n $salmon ]] || [[ $salmon != 'def' ]]
+then
+    export PATH="$salmon/bin:$PATH"
+fi
+
+
+####### CREATE LOG FILE FOR THIS SCRIPT #######
+
+IFS='/' read -ra sname <<< $input_r1
+sname=${sname[*]: -2:1}                                                                                     ### get sample name
+mkdir -p $output_folder/$sname/logs/						                                                ### create log folder 
+log_file=$output_folder/$sname/logs/$(basename $0)_log.txt
+touch $log_file
+
+echo "$(date)
+
+################################################################
+########### Alevin quantification of scRNA-seq reads ###########
+################################################################
+
+Salmon version : $(salmon --v)
+
+
+Run script    $(basename $0)    with args :
+
+-f : $input_r1
+-o : $output_folder
+-p  $salmon
+-l : $lib
+-args : ${args[@]}
+
+
+(see also salmon log file in $output_folder/$sname/logs)" >> $log_file
+
+
+
+
+####### RUN SALMON #######
+
+if [[ $lib = 'PE' ]]
+    then
+        input_r2=${input_r1/1.fastq/2.fastq}  	     			 ### get reads 2 files      
+        salmon quant ${args[@]} -1 $input_r1 -2 $input_r2 -o $output_folder/$sname/
+elif [[ $lib = 'SE' ]]
+    then
+        salmon quant ${args[@]} -r $input_r1 -o $output_folder/$sname/
+else
+    echo "
+
+    ERROR : lib parameter $lib is not valid. Can be either PE or SE." >> $log_file
+fi
+
+
+echo "
+
+
+
+
+
+
+
+
+
+
+
+" >> $log_file
+
--- scripts/bash/utils/merge-fastq-iric.sh
+++ scripts/bash/utils/merge-fastq-iric.sh
@@ -1,18 +1,60 @@
 #!/bin/bash
 
 path=$1 #path to raw data, e.g. $data/datasets/private/RNA-seq/bulk/DSP356/raw
+lib=${2:-"PE"}
 
 #for file in $path/Sample_*; do mv "$file" "${file#Sample_}";done;
 #for file in $path/*; do mv "$file" "${file%_N*}";done;
 
+
 samples=$(find $path -mindepth 1 -maxdepth 1 -type d)
 for s in $samples
 do
+
 	echo "Merging $(basename $s) fastq files..."
-	mkdir -p $s/unmerged
-	mv $s/*fastq.gz $s/unmerged 2>/dev/null	
-	cat $(find $s/unmerged -name "$(basename $s)*_R1_*") > $s/$(basename $s)_1.fastq.gz
-	cat $(find $s/unmerged -name "$(basename $s)*_R2_*") > $s/$(basename $s)_2.fastq.gz
+
+	if [[ $lib == "PE" ]]
+		then 
+			n=$(find $s -name "*1.fastq.gz" | wc -l)
+			if [[ $n == 1 ]]
+				then 
+					mv $s/*1.fastq.gz $s/$(basename $s)_1.fastq.gz
+					mv $s/*2.fastq.gz $s/$(basename $s)_2.fastq.gz
+					echo "No need to merge.
+
+					"
+			else
+					mkdir -p $s/unmerged
+					mv $s/*fastq.gz $s/unmerged 2>/dev/null
+					echo "$n files to merge ...
+
+					"
+					cat $(find $s/unmerged -name "*1.fastq.gz") > $s/$(basename $s)_1.fastq.gz
+					cat $(find $s/unmerged -name "*2.fastq.gz") > $s/$(basename $s)_2.fastq.gz
+			fi
+
+	elif [[ $lib == "SE" ]]
+		then 
+			n=$(find $s -name "*.fastq.gz" | wc -l)
+			if [[ $n == 1 ]]
+				then 
+					mv $s/*.fastq.gz $s/$(basename $s).fastq.gz
+					echo "No need to merge.
+
+					"
+			else
+					mkdir -p $s/unmerged
+					mv $s/*fastq.gz $s/unmerged 2>/dev/null
+					echo "$n files to merge ...
+
+					"
+					cat $(find $s/unmerged -name "*.fastq.gz") > $s/$(basename $s).fastq.gz
+			fi
+
+	else
+		echo "Uncorrect lib parameter."
+	fi
+
 done
 
 
