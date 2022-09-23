84f69a29ece242dd0d0bb21aa995772becfb8ff8
Marjolaine28 <marjolaine.david28@gmail.com>
2022-09-22 20:26:16 -0400

    modified get-data

--- get-data.sh
+++ get-data.sh
@@ -12,6 +12,7 @@ key=${6:-"2212-e9fcdc80cfd658683c786d89297a28fd"}
 
 
 
+
 #################################################################################
 ######################### PREPARE WORKING ENVIRONNEMENT #########################
 #################################################################################
@@ -43,10 +44,11 @@ arrayGet() {
 
 
 
-########################################################################
-############################## GET FASTQS ##############################  
-########################################################################
 
+
+##################################################################################
+############################## DOWNLOAD IRIC FASTQS ##############################  
+##################################################################################
 
 project_IDs=(762 779 992 1090)
 
@@ -60,6 +62,66 @@ project_IDs=(762 779 992 1090)
 #     merge-fastq-iric.sh $data_path/dsp$p/downloaded $data_path/dsp$p/raw-fastqs
 
 # done
+
+
+
+
+
+
+
+
+
+#########################################################################################################
+############################## DOWNLOAD REFERENCE GENOME AND TRANSCRIPTOME ##############################
+#########################################################################################################
+
+assembly=human/assembly__GRCh38-hg38
+annot=annotation__gencode/gencode_34
+
+if [[ ! -d $(git root)/references ]]; 
+then 
+    mkdir -p $(git root)/references
+
+    wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_34/gencode.v34.transcripts.fa.gz -P $(git root)/references/$assembly/$annot
+    wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_34/GRCh38.primary_assembly.genome.fa.gz -P $(git root)/references/$assembly
+else
+    echo "References already available."
+fi
+
+echo ">EGFP
+ATGGTGAGCAAGGGCGAGGAGCTGTTCACCGGGGTGGTGCCCATCCTGGTCGAGCTGGACGGCGACGTAAACGGCCACAA
+GTTCAGCGTGTCCGGCGAGGGCGAGGGCGATGCCACCTACGGCAAGCTGACCCTGAAGTTCATCTGCACCACCGGCAAGC
+TGCCCGTGCCCTGGCCCACCCTCGTGACCACCCTGACCTACGGCGTGCAGTGCTTCAGCCGCTACCCCGACCACATGAAG
+CAGCACGACTTCTTCAAGTCCGCCATGCCCGAAGGCTACGTCCAGGAGCGCACCATCTTCTTCAAGGACGACGGCAACTA
+CAAGACCCGCGCCGAGGTGAAGTTCGAGGGCGACACCCTGGTGAACCGCATCGAGCTGAAGGGCATCGACTTCAAGGAGG
+ACGGCAACATCCTGGGGCACAAGCTGGAGTACAACTACAACAGCCACAACGTCTATATCATGGCCGACAAGCAGAAGAAC
+GGCATCAAGGTGAACTTCAAGATCCGCCACAACATCGAGGACGGCAGCGTGCAGCTCGCCGACCACTACCAGCAGAACAC
+CCCCATCGGCGACGGCCCCGTGCTGCTGCCCGACAACCACTACCTGAGCACCCAGTCCGCCCTGAGCAAAGACCCCAACG
+AGAAGCGCGATCACATGGTCCTGCTGGAGTTCGTGACCGCCGCCGGGATCACTCTCGGCATGGACGAGCTGTACAAGTAA" > $references_path/exogenous/EGFP/genome.EGFP.fa
+gzip $references_path/exogenous/EGFP/genome.EGFP.fa
+cat $references_path/$assembly/$annot/gencode.v34.transcripts.fa.gz $references_path/exogenous/EGFP/genome.EGFP.fa.gz > $references_path/$assembly/$annot/gencode.v34.EGFP_transcripts.fa.gz
+
+
+
+
+
+
+
+
+
+##################################################################################
+######################## GET TRANSCRIPTS TO GENES MAPPING ########################
+##################################################################################
+
+zcat $references_path/$assembly/$annot/gencode.v34.transcripts.fa.gz | grep '>' | cut -d"|" -f1,2 > $references_path/$assembly/$annot/gencode.v34.transcripts_txp2gene.tsv
+sed -i 's/|/\t/g' $references_path/$assembly/$annot/gencode.v34.transcripts_txp2gene.tsv
+sed -i 's/>//g' $references_path/$assembly/$annot/gencode.v34.transcripts_txp2gene.tsv
+
+echo -en 'EGFP\tEGFP' > $references_path/$assembly/$annot/tmp.tsv
+cat $references_path/$assembly/$annot/gencode.v34.transcripts_txp2gene.tsv $references_path/$assembly/$annot/tmp.tsv > $references_path/$assembly/$annot/gencode.v34.EGFP_transcripts_txp2gene.tsv
+rm $references_path/$assembly/$annot/tmp.tsv
+
+
 
 
 
@@ -203,9 +265,12 @@ done
 
 
 
-# ##########################################################################
-# ############################## WHITELISTING ##############################
-# ##########################################################################
+
+
+
+##########################################################################
+############################## WHITELISTING ##############################
+##########################################################################
 
 
 
