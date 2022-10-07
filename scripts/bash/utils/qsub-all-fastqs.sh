71b3461ea074e46d8bdbb99fc2b72743e705c94f
Marjolaine28 <marjolaine.david28@gmail.com>
2022-10-07 01:13:08 -0400

    Replaced asub-all-sample

--- scripts/bash/utils/qsub-all-fastqs.sh
+++ scripts/bash/utils/qsub-all-fastqs.sh
@@ -20,7 +20,7 @@ Script    $(basename $0)    must be run with args :
 
 -m : mem = memory required for the job, e.g. 20GB
 
--h : n when the job must wait for the n-th previous task to complete (hold status), else 0
+-h : pids of the jobs that must complete before queueing this one (pids must be separted by a space ' ')
 
 -r : script to submit with qsub, e.g. run-cutadapt.sh
 
@@ -54,21 +54,26 @@ do
         -w) walltime="$1"; shift;;        
         -m) mem="$1"; shift;;            
         -h) hold="$1"; shift;;
-	-r) run_path="$1"; shift;;  
+	    -r) run_path="$1"; shift;;  
         -p) pipeline="$1"; shift;;         
         -f) input_path="$1"; shift;;            
         -o) output_path="$1"; shift;;
-	-l) lib="$1"; shift;;
+    	-l) lib="$1"; shift;;
         -s) samples="$1"; IFS=' ' declare -a 'samples=($samples)'; shift;;
         -args) args="$1"; shift;;
     esac
 done
 
 lib=${lib:-'PE'}
-hold=${hold:-'0'}
+hold=${hold:-''}
 samples=${samples:-'all'}  
 pipeline=${pipeline:-'def'}
-hold=${hold:-'0'}
+
+function join_by { local d=${1-} f=${2-}; if shift 2; then printf %s "$f" "${@/#/$d}"; fi; }
+
+IFS=' ' declare -a 'hold=($hold)'
+
+# pids=$(join_by : ${hold[@]})
 
 
 ####### CACLCULATE TOTAL NUMBER OF SAMPLES (INCLUDING REPLICATES) THAT WILL BE PROCESSED #######
@@ -77,9 +82,9 @@ if [[ $samples = 'all' ]]
 then
     if [[ $lib = SE ]]
     then
-        S=($(find $input_path -maxdepth 2 -name *.fastq*))
+        S=($(find $input_path -maxdepth 2 -name "*.fastq*"))
     else
-        S=($(find $input_path -maxdepth 2 -name *r1.fastq*))
+        S=($(find $input_path -maxdepth 2 -name "*R1.fastq*"))
     fi
 else
     S=()
@@ -87,15 +92,14 @@ else
     do
             if [[ $lib = SE ]]
             then
-                    rep=($(find $input_path/$s/ -maxdepth 1 -name *.fastq*))
+                    rep=($(find $input_path/$s/ -maxdepth 1 -name "*.fastq*"))
             else
-                    rep=($(find $input_path/$s/ -maxdepth 1 -name *r1.fastq*))
+                    rep=($(find $input_path/$s/ -maxdepth 1 -name "*R1.fastq*"))
                     S=(${S[@]} $rep)
             fi
     done
 fi
 tot=${#S[@]}
-
 
 
 ###### CREATE A LOG FILE FOR THIS SCRIPT #######
@@ -140,64 +144,33 @@ Run script    $(basename $0)    with args :
 
 run=$(basename $run_path) 
 
-
-if [[ $hold != 0 ]]
-then
-        IFS=' ' declare -a 'hold=($hold)'
-        J=''
-        for hi in ${hold[@]}
-        do 
-                if [[ ${hi:1:2} != n ]] && [[ ${hi:0:1} != 0 ]]
-                then    
-                        n=${hi:0:1}
-                        c=0
-                        for hj in ${hold[@]}
-                        do
-                                if [[ ${hj:1:2} = n ]] && [[ ${hj:0:1} != 0 ]] && [[ ${hj:0:1} < ${hi:0:1} ]]
-                                then
-                                        n=$(($n-1))
-                                        c=$(($c+$tot))
-                                fi
-                        done
-                        J=$J:$(qstat -u $USER | tail -n $(($n+$c)) | head -n 1 | awk '{print $1}' | cut -d"." -f1)
-                fi
-        done
-fi
-
+i=$((${#S[@]}-1))
 
 for s in ${S[@]}
 do
-        IFS='/' read -ra sname <<< $s
-        sname=${sname[*]: -2:1}                                                                                                 ### get sample name
-        mkdir -p $output_path/$sname/logs/						                                        ### create log folder 
-        # find  $output_path/$sname/ -mindepth 1 -maxdepth 1 -name '*fastq*' -exec rm -r 2>/dev/null "{}" \;	                ### remove existing results
-        $run_path -emp 1 -f $s -o $output_path -l $lib -p $pipeline                                                        ### it is useful to create empty files and folder for job with hold status (they take output of previous task as input, but if the output is not available yet the holding task would have no input)
-		
-        if [[ $hold = 0 ]]                                                                                                      ### submit jobs ; hold option allows to wait for some previous task to finish before exection
-        then    
-                echo "$run_path -f $s -o $output_path -p $pipeline -args '$args' -l $lib" | qsub -V -l nodes=1,mem=$mem,vmem=$mem,walltime=$walltime -j oe -d $output_path/${sname}/logs/ -N "${run}_${sname}"
-        else	
-                W=$J				
-                for hj in ${hold[@]}
-                do 
-                        if [[ ${hj:1:2} = n ]] && [[ ${hj:0:1} != 0 ]]
-                        then
-                                n=${hj:0:1}
-                                c=0
-                                for hi in ${hold[@]}
-                                do
-                                        if [[ ${hi:1:2} != n ]] && [[ ${hi:0:1} != 0 ]] && [[ ${hi:0:1} < ${hj:0:1} ]]
-                                        then
-                                                n=$(($n-1))
-                                                c=$(($c+1))
-                                        fi
-                                done
-                                W=$W:$(qstat -u $USER | tail -n $(($tot*$n+$c)) | head -n 1 | awk '{print $1}' | cut -d"." -f1)
-                        fi
-                done
-                W=${W:1}
-                echo "Wait job $W to complete before queueing :"
-                echo "$run_path -f $s -o $output_path -p $pipeline -args '$args' -l $lib" | qsub -V -l nodes=1,mem=$mem,vmem=$mem,walltime=$walltime -j oe -d $output_path/${sname}/logs/ -N "${run}_${sname}" -W depend=afterok:$W
-                echo "(Hold status)"
-        fi
+    IFS='/' read -ra sname <<< $s
+    sname=${sname[*]: -2:1}                                                                                                 ### get sample name
+    mkdir -p $output_path/$sname/logs/						                                                                ### create log folder 
+    $run_path -emp 1 -f $s -o $output_path -l $lib -p $pipeline; ec=$?                                                      ### it is useful to create empty files and folder for job with hold status (they take output of previous task as input, but if the output is not available yet the holding task would have no input)
+
+    if [[ $ec == 1 ]]; 
+    then
+        echo "Job not submitted for $s."
+        continue
+    fi
+
+    if [[ $hold = "" ]]                                                                                                     ### submit jobs ; hold option allows to wait for some previous task to finish before exection
+    then
+        echo "$run_path -f $s -o $output_path -p $pipeline -args '$args' -l $lib" | qsub -V -l nodes=1,mem=$mem,vmem=$mem,walltime=$walltime -j oe -d $output_path/${sname}/logs/ -N "${run}_${sname}"
+    else
+        IFS=' ' declare -a 'hold=($hold)'
+        pids=""; 
+        for h in ${hold[@]}; do if [[ ${h:0:1} == n ]]; then pids="$pids $((${h:1} - $i))"; else pids="$pids $h"; fi; done;
+        IFS=' ' declare -a 'pids=($pids)'; 
+        pids=$(join_by : ${pids[@]}); 
+        echo "Wait jobs $pids to complete before queueing :"
+        echo "$run_path -f $s -o $output_path -p $pipeline -args '$args' -l $lib" | qsub -V -l nodes=1,mem=$mem,vmem=$mem,walltime=$walltime -j oe -d $output_path/${sname}/logs/ -N "${run}_${sname}" -W depend=afterok:$pids
+        echo "(Hold status)"
+        i=$(($i-1))
+    fi
 done
